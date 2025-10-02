04ce7e44f352cea6fca5d93a37e570b5c612a0cd
fersko21 <felipeko021@gmail.com>
2025-10-02 17:30:41 -0300

    Dev: usar SQLite temporalmente para continuar desarrollo del CRUD

--- proyecto/inventario-quilhuica/gestion/settings.py
+++ proyecto/inventario-quilhuica/gestion/settings.py
@@ -76,12 +76,8 @@ WSGI_APPLICATION = "gestion.wsgi.application"
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'quilhuica_db',     # el nombre de la base que creaste
-        'USER': 'postgres',         # el usuario con el que probaste psql
-        'PASSWORD': 'admin123456', # la misma que usaste recién en psql
-        'HOST': 'localhost',        # o 127.0.0.1
-        'PORT': '5432',
+         "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
