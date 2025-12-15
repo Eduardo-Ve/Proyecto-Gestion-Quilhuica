75511a877a20bd339e673626edbe412ada4083e0
Eduardo Velasquez <107139575+Eduardo-Ve@users.noreply.github.com>
2025-12-15 02:51:53 -0300

    Update ALLOWED_HOSTS and CSRF_TRUSTED_ORIGINS config

--- inventario-quilhuica/gestion/gestion/settings.py
+++ inventario-quilhuica/gestion/gestion/settings.py
@@ -25,13 +25,9 @@ SECRET_KEY =  config('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['gestion-quilhuica.online', 'www.gestion-quilhuica.online',]  
-
+ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="").split(",")
 #Trusted Origins
-CSRF_TRUSTED_ORIGINS = [
-    "https://gestion-quilhuica.online",
-    "https://www.gestion-quilhuica.online",
-]
+CSRF_TRUSTED_ORIGINS = config("CSRF_TRUSTED_ORIGINS", default="").split(",")
 
 
 # Application definition
